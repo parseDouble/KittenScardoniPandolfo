17dd8264b9525d52ec21c9683fa959e24458f412
Fausto Spoto <fausto.spoto@univr.it>
2015-03-23 23:21:36 +0100

    Added try with resource

--- src/syntactical/Generator.java
+++ src/syntactical/Generator.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 public class Generator {
 
 	public static void main(String[] args) throws IOException, Exception {
-		FileInputStream fis = new FileInputStream("resources/Kitten.cup");
-		System.setIn(fis);
-		java_cup.Main.main(new String[] { "-parser", "Parser" });
-		fis.close();
+		try (FileInputStream fis = new FileInputStream("resources/Kitten.cup")) {
+			System.setIn(fis);
+			java_cup.Main.main(new String[] { "-parser", "Parser" });
+		}
 	}
 }
